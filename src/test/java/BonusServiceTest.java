b4ba03a5c23d0c0aebfcf815198f05f9a232037e
Dmitri Romanov <investdjr@gmail.com>
2022-05-11 20:40:26 +0300

    commited

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusServiceTest {
+
+    @Test
+    public void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(1000_60, true);
+        long expected = 30;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(1_000_000_60, true);
+        long expected = 500;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
